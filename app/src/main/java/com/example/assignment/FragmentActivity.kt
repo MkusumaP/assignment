df6db1795d407dcb252b61cdcb9c235f5102a9fa
MkusumaP <mkusumapriyanka@deloitte.com>
2022-02-23 16:45:41 +0530

    Retrofit example

--- app/src/main/java/com/example/assignment/FragmentActivity.kt
+++ app/src/main/java/com/example/assignment/FragmentActivity.kt
@@ -2,14 +2,28 @@ package com.example.assignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.FragmentTransaction
 import com.example.assignment.ui.main.Fragment
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import retrofit2.create
 
 class FragmentActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_activity)
+
+val quotesApi = RetrofitHelper.getInstance().create(QuotesApi::class.java)
+
+        GlobalScope.launch {
+            val response = quotesApi.getQuotes()
+            val quotesList = response.body()
+            if (quotesList != null)
+                Log.d("kusuma: ", quotesList.toString())
+        }
+
         if (savedInstanceState == null) {
             val tag = "Fragment1"
             val transaction = supportFragmentManager.beginTransaction()
